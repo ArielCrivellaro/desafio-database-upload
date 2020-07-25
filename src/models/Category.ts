1a5ca91e2a8f97c616a806e36c97c1eb094ebcf4
Ariel Crivellaro Martins <arielcrivellaro@gmail.com>
2020-07-25 13:32:22 -0300

    Desafio Finalizado

--- src/models/Category.ts
+++ src/models/Category.ts
@@ -1,10 +1,23 @@
+import{
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('categories')
 class Category {
+  @PrimaryGeneratedColumn("uuid")
   id: string;
 
+  @Column()
   title: string;
 
+  @UpdateDateColumn()
   created_at: Date;
 
+  @CreateDateColumn()
   updated_at: Date;
 }
 
